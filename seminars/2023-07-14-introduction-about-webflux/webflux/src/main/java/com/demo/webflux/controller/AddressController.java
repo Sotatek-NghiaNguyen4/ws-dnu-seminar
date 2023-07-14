274b049537fe2ec5e0ee0dfcfab828c263e06224
JasonLeSotatek <e.tho.le@wesang.com>
2023-07-14 09:17:12 +0700

    Finish

--- seminars/2023-07-14-introduction-about-webflux/webflux/src/main/java/com/demo/webflux/controller/AddressController.java
+++ seminars/2023-07-14-introduction-about-webflux/webflux/src/main/java/com/demo/webflux/controller/AddressController.java
@@ -1,11 +1,28 @@
 package com.demo.webflux.controller;
 
+import com.demo.webflux.model.Address;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.awt.*;
+import java.time.Duration;
+import java.util.List;
 
 @RestController
 @RequestMapping("/address")
 public class AddressController {
 
+    @GetMapping("/v1/get-all-address")
+    public List<String> getAllAddress() throws InterruptedException {
+        Thread.sleep(5000);
+        return List.of("Ha Noi", "Hai Duong", "Kon Tum", "Thai Nguyen", "Hung Yen");
+    }
 
+    @GetMapping(value = "/v2/get-all-address")
+    public Flux<String> getAllAddressWithReactive() {
+        return Flux.just("Ha Noi", "Hai Duong", "Kon Tum", "Thai Nguyen", "Hung Yen").delayElements(Duration.ofSeconds(1));
+    }
 }
